4898c774b8b432c681781475e13b0641014936fe
Start <poojaumangal@gmail.com>
2023-11-21 20:14:33 +0530

    Final Commit

--- Soup.py
+++ Soup.py
@@ -1,9 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 res = requests.get("https://news.ycombinator.com/")
 Soup = BeautifulSoup(res.text, 'html.parser')
 
 print(Soup.select('#score_38331097'))
-links = Soup.select('.age')
+links = Soup.select('.titleline > a')
+votes = Soup.select('.score')
+subtext = Soup.select('.subtext')
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(votes[idx].getText().replace('points'," "))
+            if points > 199:
+                hn.append({'title': title, 'link' :href, 'votes': points })
+    return hn
+pprint.pprint(create_custom_hn(links, subtext))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
